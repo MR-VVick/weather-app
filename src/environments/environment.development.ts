09356140140fbcc37ec5de72d630500a552c3aaa
Sahil Amin <sahilamin68@gmail.com>
2024-06-10 21:41:47 +0530

    apiurl changed http to https

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     apiKey: '04fb667865e7497092663228240906',
-    apiUrl: 'http://api.weatherapi.com/v1/'
+    apiUrl: 'https://api.weatherapi.com/v1/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiKey: '04fb667865e7497092663228240906',
-    apiUrl: 'http://api.weatherapi.com/v1/'
+    apiUrl: 'https://api.weatherapi.com/v1/'
 };
